9c93adffd7576de480bbae554db47238b03717bc
Habibur Rahman <habib.swi16@gmail.com>
2023-09-13 16:20:42 +0600

    fetchadata

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 import "./Home.css";
 
 const Home = () => {
+  const [allActors, setAllActors] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("./data.json");
+      const data = await res.json();
+      setAllActors(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="container">
       <div className="card-container">
@@ -12,20 +25,26 @@ const Home = () => {
               className="photo"
             />
           </div>
+
           <h2>Antony</h2>
+
           <p>
             <small>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
               quos!
             </small>
           </p>
+
           <div className="info">
             <p>salary:200</p>
+
             <p>Writer</p>
           </div>
+
           <button className="card-btn">Select</button>
         </div>
       </div>
+      .cart
     </div>
   );
 };
